d8bc9f7371c55c30c3b768c9bea91e8c60e9cc83
Prashanth Kumar <prash8669@gmail.com>
2020-10-28 23:24:43 +0530

    updated circle.js a per the requirement

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,5 +1,16 @@
 // import using require
+import Shape from "./shape";
 
+export default class Circle extends Shape{
+    constructor() {
+        super();
+    }
+    
+    calculateArea() {
+        console.log("Circel Area with color " + this.color + " calculated");
+        return 3.14 * 5 * 5;
+    }
+}
 // declare class
 
 
