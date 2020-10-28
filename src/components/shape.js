8b9e56b39827b0c64533ef5a634f588ee5812bd8
Prashanth Kumar <prash8669@gmail.com>
2020-10-28 23:24:10 +0530

    updated shape.js as per the requirement

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+export default class Shape {
+    constructor() {
+        this.color = "red";
+    }
 
+    drawShape() {
+        console.log("Shape drawn");
+    }
+
+    calculateArea() {
+        console.log("Shape area calculated");
+    }
+}
 // export class using module.exports
